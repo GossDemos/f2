129d35b3da16a65a197f295d3fcd92df8ec0e4ee
Ilya Goss <goss@corp.ifmo.ru>
2019-02-20 14:52:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const d = dirname(require.main.filename);
 readdir(d, (err, filenames) => {
     if (err) { console.log('bad'); process.exit(1) };
     filenames.forEach(filename => {
-        readFile(filename, (err2, r2) => {
-            console.log(String(r2));
+        readFile(filename, (err2, text) => {
+            console.log(String(text));
         })
-    }); // массив r доступен здесь, а не снаружи
+    }); 
 });
